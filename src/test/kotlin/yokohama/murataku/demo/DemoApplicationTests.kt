a2509fc1d95ded3f8de60565146018dd57ffa64b
Takuya "Mura-Mi" Murakami <mura.mi.nantoka@gmail.com>
2020-12-12 21:05:15 +0900

    init by spring initializr

---
+++ src/test/kotlin/yokohama/murataku/demo/DemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package yokohama.murataku.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
